858b6e74187fb3af17dbda04162c7209f454c090
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-14 20:43:28 -0300

    Add middleware and integrate Poseidon routes
    Added middleware to handle JSON and URL-encoded form data. Integrated Poseidon routes into the application.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 
+// Habilita receber JSON e dados de formulários
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Integra as rotas do módulo Poseidon
+app.use("/poseidon", require("./poseidon/poseidonroutes"));
+
+// Rota principal
 app.get("/", (req, res) => {
   res.send("Servidor Node ativo.");
 });
 
+// Porta usada pelo Render
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Servidor Node rodando na porta " + PORT);
 });
