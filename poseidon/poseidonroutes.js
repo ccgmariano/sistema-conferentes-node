7c464c2b50346f7c6df72d04ced299eb25db9960
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-15 10:02:43 -0300

    Implement health check route
    Added health check route to Poseidon routes.

--- poseidon/poseidonroutes.js
+++ poseidon/poseidonroutes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
+
 const controller = require("./poseidoncontroller");
 
+// rota de verificação (health check)
+router.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    msg: "Poseidon routes ativas."
+  });
+});
+
+// rota de teste do login Poseidon
 router.get("/teste", controller.teste);
 
 module.exports = router;
